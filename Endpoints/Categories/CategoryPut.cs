c6dfc118abd48dc89799f4875b8120b88d43356b
Alexandre Oliveira <ale.devx@gmail.com>
2023-01-27 13:45:59 -0400

    Concluído primeiros passos do projeto

---
+++ Endpoints/Categories/CategoryPut.cs
@@ -0,0 +1,33 @@
+﻿using IWantApp.Infra.Data;
+using Microsoft.AspNetCore.Mvc;
+using static System.Net.WebRequestMethods;
+
+namespace IWantApp.Endpoints.Categories;
+
+public class CategoryPut
+{
+    public static string Template => "/categories/{id}";
+    public static string[] Methods => new string[] { HttpMethod.Put.ToString() };
+    public static Delegate Handle => Action;
+
+    public static IResult Action(
+        [FromRoute] Guid id,
+        CategoryRequest categoryRequest,
+        ApplicationDbContext context) {
+
+        var category = context.Categories.Where(c => c.Id == id).FirstOrDefault();
+
+        if (category == null)
+            return Results.NotFound();
+
+        category.Name = categoryRequest.Name;
+        category.Active = categoryRequest.Active;
+
+        context.Categories.Update(category);
+        context.SaveChanges();
+
+        return Results.Ok();
+
+
+    }
+}
