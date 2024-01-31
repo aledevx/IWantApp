cbecee3f2c447401238dd98a0989291371ef96f3
Alexandre Oliveira <ale.devx@gmail.com>
2024-01-30 21:05:36 -0400

    Módulo 'Vitrine de produtos' finalizado

---
+++ Endpoints/Products/ProductGetShowcase.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Claims;
+using IWantApp.Endpoints.Products;
+using IWantApp.Infra.Data;
+
+namespace IWantApp.Endpoints.Employees;
+
+public class ProductGetShowcase
+{
+    public static string Template => "/products/showcase";
+    public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
+    public static Delegate Handle => Action;
+
+    [AllowAnonymous]
+    public static IResult Action(ApplicationDbContext context, int page = 1, int row = 10, string orderBy = "name")
+    {
+
+        if (row > 10)
+            return Results.Problem(title: "Row with max 10", statusCode: 400);
+
+        var queryBase = context.Products.AsNoTracking().Include(p => p.Category)
+            .Where(p => p.HasStock && p.Category.Active);
+
+
+        if (orderBy == "name")
+            queryBase = queryBase.OrderBy(p => p.Name);
+        else if (orderBy == "price")
+            queryBase = queryBase.OrderBy(p => p.Price);
+        else
+            return Results.Problem(title: "Order only by price or name", statusCode: 400);
+
+        var queryFilter = queryBase.Skip((page - 1) * row).Take(row);
+
+        var products = queryFilter.ToList();
+
+        var result = products.Select(p => new ProductResponse(p.Name, p.Category.Name, p.Description, p.HasStock, p.Price, p.Active));
+        return Results.Ok(result);
+    }
+}
